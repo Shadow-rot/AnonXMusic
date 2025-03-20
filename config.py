ea8c980dd178e6ee633a7a2f622c3be296c8e349
Shadow-rot <looktouhid@gmail.com>
2025-03-21 05:00:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -84,7 +84,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/ydjas6.mp4"
+    "START_IMG_URL", "https://files.catbox.moe/0o2irw.mp4"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/9vyq2q.jpg"
