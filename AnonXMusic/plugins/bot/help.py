eafed5576c872702d7919f4d9126f8a2e6ba462b
Shadow-rot <looktouhid@gmail.com>
2025-03-28 04:38:43 +0530

    Update help.py

--- AnonXMusic/plugins/bot/help.py
+++ AnonXMusic/plugins/bot/help.py
@@ -4,7 +4,7 @@ from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 from AnonXMusic import app
-from AnonXMusic.utils import help_pannel
+from AnonXMusic.utils import private_help_pannel
 from AnonXMusic.utils.database import get_lang
 from AnonXMusic.utils.decorators.language import LanguageStart, languageCB
 from AnonXMusic.utils.inline.help import ( 
