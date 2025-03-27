69624698372919c19f9cf98ceeea01d7470c286e
Shadow-rot <looktouhid@gmail.com>
2025-03-28 04:48:46 +0530

    Update help.py

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -14,7 +14,7 @@ def generate_help_buttons(_, start: int, end: int, current_page: int):
     return buttons
 
 def first_page(_):
-    """First Help Page """
+    """Help - Page 1 """
     buttons = generate_help_buttons(_, start=1, end=15, current_page=1)
     navigation_buttons = [
         InlineKeyboardButton(text="๏ ᴍᴇɴᴜ ๏", callback_data="back_to_main"),
@@ -24,7 +24,7 @@ def first_page(_):
     return InlineKeyboardMarkup(buttons)
 
 def second_page(_):
-    """Second Help Page """
+    """Help - Page 2"""
     buttons = generate_help_buttons(_, start=16, end=30, current_page=2)
     navigation_buttons = [
         InlineKeyboardButton(text="๏ ʙᴀᴄᴋ ๏", callback_data="help_prev"),
