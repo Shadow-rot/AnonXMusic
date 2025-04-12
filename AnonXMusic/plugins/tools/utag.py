8d4c1f7d8d948a83b2d097dadd5278284a5b332d
Shadow-rot <looktouhid@gmail.com>
2025-04-12 08:47:30 +0530

    Update utag.py

--- AnonXMusic/plugins/tools/utag.py
+++ AnonXMusic/plugins/tools/utag.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.errors import FloodWait, UserNotParticipant
 from AnonXMusic import app
-from AnonXMusic.utils.sha_ban import admin_filter
+from AnonXMusic.utils.shadwo_ban import admin_filter
 
 spam_chats = []
 
@@ -12,7 +12,7 @@ async def tag_all_users(client, message):
     chat_id = message.chat.id
 
     if chat_id in spam_chats:
-        return await message.reply("Tagging is already in progress...")
+        return await message.reply("**Tagging is already in progress...**")
 
     spam_chats.append(chat_id)
 
@@ -28,7 +28,8 @@ async def tag_all_users(client, message):
                 break
 
             user = member.user
-            if user.is_bot:
+            # Skip bots and deleted users
+            if user.is_bot or user.is_deleted:
                 continue
 
             batch.append(f"⊚ {user.mention}")
@@ -50,7 +51,7 @@ async def send_batch(client, chat_id, replied, text, batch):
     tag_text = f"{text}\n" + "\n".join(batch)
     try:
         if replied:
-            await replied.reply_text(tag_text, quote=True, disable_web_page_preview=True)
+            await replied.reply(tag_text, quote=True, disable_web_page_preview=True)
         else:
             await client.send_message(chat_id, tag_text, disable_web_page_preview=True)
     except FloodWait as e:
@@ -68,9 +69,9 @@ async def cancel_spam(client, message):
     try:
         member = await client.get_chat_member(chat_id, message.from_user.id)
         if member.status not in (ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER):
-            return await message.reply("You must be an admin to stop tagging.")
+            return await message.reply("**You must be an admin to stop tagging.**")
     except UserNotParticipant:
-        return await message.reply("You must be in the group to stop tagging.")
+        return await message.reply("**You must be in the group to stop tagging.**")
 
     spam_chats.remove(chat_id)
-    return await message.reply("Tagging has been stopped.")
+    return await message.reply("**Tagging has been stopped.**")
