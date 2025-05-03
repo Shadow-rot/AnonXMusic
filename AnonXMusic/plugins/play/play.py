477be7c280dd03bd34045dcad9f9d910dc88c938
Shadow-rot <looktouhid@gmail.com>
2025-05-03 21:12:32 +0530

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -36,6 +36,7 @@ from config import BANNED_USERS, lyrical
             "vplayforce",
             "cplayforce",
             "cvplayforce",
+prefixes=["/", "!", "."]
         ]
     )
     & filters.group
